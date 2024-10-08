2cf9957675e37d30d28214a8021afcd738e482a9
jayc595 <jay.crocker@hotmail.co.uk>
2024-10-08 20:25:59 +0100

    Resolves issue with docker container + fixes nextjs hotswapping

--- meltano-ui/src/app/api/getMeltanoProjects/route.js
+++ meltano-ui/src/app/api/getMeltanoProjects/route.js
@@ -3,8 +3,9 @@ import path from 'path';
 
 export async function GET(req) {
   try {
+    
     // Define the path to the meltano projects folder
-    const meltanoProjectsPath = path.join(process.cwd(), '../meltano'); // Adjust the path as necessary
+    const meltanoProjectsPath = path.join('/app/meltano');
 
     // Read the directory contents
     const directories = await fs.readdir(meltanoProjectsPath, { withFileTypes: true });
@@ -30,7 +31,7 @@ export async function GET(req) {
     });
   } catch (error) {
     console.error('Error reading directory:', error);
-    return new Response(JSON.stringify({ error: 'Failed to read directory' }), {
+    return new Response(JSON.stringify({ error: 'Failed to read directoryssss' }), {
       status: 500,
     });
   }
