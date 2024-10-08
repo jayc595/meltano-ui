59392af05ab0a67e824c03a311aee647693e58b5
jayc595 <jay.crocker@hotmail.co.uk>
2024-10-08 20:26:44 +0100

    Fixes typo

--- meltano-ui/src/app/api/getMeltanoProjects/route.js
+++ meltano-ui/src/app/api/getMeltanoProjects/route.js
@@ -31,7 +31,7 @@ export async function GET(req) {
     });
   } catch (error) {
     console.error('Error reading directory:', error);
-    return new Response(JSON.stringify({ error: 'Failed to read directoryssss' }), {
+    return new Response(JSON.stringify({ error: 'Failed to read directory' }), {
       status: 500,
     });
   }
